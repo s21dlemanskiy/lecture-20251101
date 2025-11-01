f92c56a7bcba801cb33205dc9062336b82f7b62f
Dmitry Romanov <me@wtrn.ru>
2025-11-01 14:39:01 +0300

    Add basic valkey example

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import valkey
+
+valkey_connection = valkey.Valkey(host='localhost', port=6379, db=0)
+
 def main():
-    print("Hello from lecture-20251101!")
+    valkey_connection.lpush("test_queue", "Hello")
+    print("Push successful!")
 
 
 if __name__ == "__main__":
